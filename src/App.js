5e25807d8e2383fcb167d4529801d37efaf458b8
Himanshu Chouhan <hchouhan3654@gmail.com>
2021-05-28 15:49:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,8 @@ function App() {
     const month = String(dateObj.getMonth() + 1).padStart(2, "0");
     const day = String(dateObj.getDate()).padStart(2, "0");
     const year = dateObj.getFullYear();
-    const output = day + "/" + month + "/" + year;
+    //Fixing the date picker.
+    const output = month + "/" + day + "/" + year;
     setSelectedDate(output);
   };
   const handleSubmit = (e) => {
